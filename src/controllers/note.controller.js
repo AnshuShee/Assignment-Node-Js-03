34289e6e99fa99a4188a0de47231c7dd2f378701
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-03 12:05:06 +0530

    feat: add search all fields endpoint (GET /api/notes/search/all)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -371,6 +371,41 @@ const searchByContent = async (req, res) => {
   }
 };
 
+// 11. GET /api/notes/search/all - Search in title AND content
+const searchAll = async (req, res) => {
+  try {
+    const { q } = req.query;
+
+    if (!q) {
+      return res.status(400).json({
+        success: false,
+        message: "Search query 'q' is required",
+        data: null,
+      });
+    }
+
+    const notes = await Note.find({
+      $or: [
+        { title: { $regex: q, $options: "i" } },
+        { content: { $regex: q, $options: "i" } },
+      ],
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Search results for: ${q}`,
+      count: notes.length,
+      data: notes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
 module.exports = {
   createNote,
   createBulkNotes,
@@ -382,4 +417,5 @@ module.exports = {
   deleteBulkNotes,
   searchByTitle,
   searchByContent,
+  searchAll,
 };
